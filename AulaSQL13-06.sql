85b39f6f7d7bc7ad070da5f132de17adf6aea81d
João Magno <162519556+joaomagno1@users.noreply.github.com>
2025-06-13 21:59:03 -0300

    upxd^^

--- AulaSQL13-06.sql
+++ AulaSQL13-06.sql
@@ -6,17 +6,17 @@ SELECT count(CustomerName) FROM Customers;
 
 SELECT count(distinct Country) FROM Customers;
 
-*/3. Calcular a média dos preços dos produtos.*/
+/*3. Calcular a média dos preços dos produtos.*/
 
 SELECT avg(Price) as preco_media FROM Products;
 
-*/4. Consultar o maior e o menor preço de produto.*/
+/*4. Consultar o maior e o menor preço de produto.*/
 
 SELECT max(Price)  'Maior Preco',
 	min(Price) 'MenorPreco'
 FROM Products;
 
-*/5. Calcular a quantidade de clientes que são brasileiros. O título da consulta deve ser ‘Quantidade de Brasileiros’.*/
+/*5. Calcular a quantidade de clientes que são brasileiros. O título da consulta deve ser ‘Quantidade de Brasileiros’.*/
 	
 SELECT count(Country) "Quantidade de Brasileiros" 
 FROM Customers
