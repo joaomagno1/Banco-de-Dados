cd94f4aa408fa4cf5bc07013280d1f1eb6afadf0
João Magno <162519556+joaomagno1@users.noreply.github.com>
2025-06-13 21:50:57 -0300

    up^^

--- AulaSQL13-06.sql
+++ AulaSQL13-06.sql
@@ -22,4 +22,43 @@ SELECT count(Country) "Quantidade de Brasileiros"
 FROM Customers
 where Country = "Brazil";
 
+/*-------------------------------------------------------//-------------------------------------------------------*/
 
+/*1. Descobrir quantos pedidos cada cliente fez. O título da consulta deve ser “ID do Cliente” e a “Quantidade de Pedidos”.*/
+
+SELECT CustomerID "ID do cliete",
+count(OrderID) "Quantidade de pedidos"
+FROM Orders
+group by CustomerID;
+
+/*2. Obter o valor médio dos produtos para cada Categoria.*/
+
+SELECT CategoryID 'Categoria do produto', 
+avg(Price) 'Média por categoria'
+FROM Products
+group by CategoryID;
+
+/*3. Apresentar o total de quantidades de produtos por pedido.*/
+
+SELECT OrderID, sum(quantity) 'Total de Qtde por pedido'
+FROM OrderDetails 
+group by OrderID;
+
+/*4. Verificar quantos fornecedores estão cadastrados por país.*/
+
+SELECT Country, count(Country) 'Fornecedores cadastrados por países'
+FROM Suppliers
+group by Country;
+
+/*5. Verificar quantos fornecedores estão cadastrados por país no qual essa quantidade tem que ser acima de 1.*/
+
+SELECT Country 'País', count(Country) 'Fornecedores cadastrados por países'
+FROM Suppliers
+group by Country
+having count(Country) > 1;
+
+/*6. Calcular a quantidade de pedidos realizadas por funcionário. */
+
+SELECT EmployeeID 'ID Funcionario', count(EmployeeID) 'Qtde de pedidos por funcionários'
+FROM Orders
+group by EmployeeID;
