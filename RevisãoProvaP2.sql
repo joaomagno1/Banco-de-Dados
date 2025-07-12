3a54df14a593c66d63b7ab94886bbe3b46007b71
João Magno <162519556+joaomagno1@users.noreply.github.com>
2025-07-11 21:50:59 -0300

    end?

--- RevisãoProvaP2.sql
+++ RevisãoProvaP2.sql
@@ -4,14 +4,14 @@ use loja_db;
 create table categorias(
 	id_categoria int primary key auto_increment,
     nome varchar(50) not null unique
-    );
+);
     
 create table lojas(
 	id_loja int auto_increment,
     nome_loja varchar(30) not null,
     cidade varchar(55) default 'Birigui',
     primary key(id_loja)
-    );
+);
 
 create table produtos(
 	id_produto int auto_increment,
@@ -25,11 +25,47 @@ create table produtos(
 );
 
 insert into categorias(nome)
-values('Alimentos'),
-	  ('Limpezas');
+	values('Alimentos'),
+		  ('Limpezas');
 
-insert into lojas(nome_loja)
+insert into lojas(nome_loja, cidade)
 values('Loja Um'),
-	  ('Loja Dois');
-      
-select * from lojas;
+	  ('Loja Dois'),
+      ('Loja Três', 'Nova Luzitânia');
+
+insert into produtos(nome_produto, preco, id_loja, id_categoria)
+values('Cavadeira', 78.99, 1, 1),
+	  ('Cimento', 58.79, 2, 2);
+
+select nome_loja, cidade
+from lojas
+where nome_loja like '%e';
+
+select * 
+from categorias
+where id_categoria in(2, 7, 8, 10);
+
+select *
+from produtos
+where id_loja = 2 and preco between 120 and 550;
+
+select p.nome_produto, p.preco
+	from produtos p join categorias c on p.id_categoria = c.id_categoria
+where c.nome = 'Alimentos'
+order by preco desc;
+
+select p.nome_produto, l.cidade
+	from produtos p join lojas l on p.id_loja = l.id_loja
+where l.cidade = 'Curitiba';
+
+select p.nome_produto, l.cidade
+	from produtos p, lojas l
+where l.cidade = 'Curitiba' and p.id_loja = l.id_loja;
+
+select p.nome_produto, p.preco, l.nome_loja, c.nome
+	from produtos p, lojas l, categorias c
+where p.id_loja = l.id_loja and p.id_categoria = c.id_categoria;
+
+select p.nome_produto, p.preco, l.nome_loja, c.nome
+	from produtos p join lojas l on p.id_loja = l.id_loja
+    				join categorias c on p.id_categoria = c.id_categoria;
